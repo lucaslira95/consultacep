57d608e4213b216fc01c20ddfc5ae7ad52905b32
Lucas Lira <lucasgta95@gmail.com>
2019-10-25 00:20:40 -0300

    initial release with bugs

---
+++ js/index.js
@@ -0,0 +1,41 @@
+document.getElementById("consultar").addEventListener("click", consultar, false);
+
+async function consultar() {
+    enableSpinner();
+    let cep = $('#cep').val();
+    await $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+        let thead = $("#tabela thead");
+        if (thead.children().length == 0) {
+            $("#tabela thead").append(
+                "<tr>" +
+                "<th scope='col'>CEP</th>" +
+                "<th scope='col'>Logradouro</th>" +
+                "<th scope='col'>Bairro</th>" +
+                "<th scope='col'>Localidade</th>" +
+                "</tr>"
+            );
+        }
+        $("#tabela tbody").append(
+            "<tr>" +
+            "<td>" + dados.cep + "</td>" +
+            "<td>" + dados.logradouro + "</td>" +
+            "<td>" + dados.bairro + "</td>" +
+            "<td>" + dados.localidade + "</td>" +
+            "</tr>"
+        );
+    });
+    disableSpinner();
+}
+
+function enableSpinner() {
+    $("#consultar").attr("disabled", true);
+    $("#consultar").html('Consultando...');
+    $("#consultar").append("<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span>");
+}
+
+function disableSpinner() {
+    $("#consultar").empty();
+    $("#consultar").attr("disabled", false);
+    $("#consultar").html('Consultar');
+}
+
