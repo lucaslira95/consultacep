590b96b5d5f49bc7f7fb491395d68e6ed46bd62e
Lucas Lira <lucasgta95@gmail.com>
2019-12-18 01:00:15 -0300

    fixed bugs and improved alert

--- js/index.js
+++ js/index.js
@@ -2,36 +2,57 @@ document.getElementById("consultar").addEventListener("click", consultar, false)
 
 async function consultar() {
     enableSpinner();
-    let cep = $('#cep').val();
-    await $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
-        if (dados.cep == undefined) {
-            alert("CEP não existente");
-        }
-        else {
-            let thead = $("#tabela thead");
-            if (thead.children().length == 0) {
-                $("#tabela thead").append(
+    let cep = $('#cep').val().replace(/\D/g, '');
+    var validacep = /^[0-9]{8}$/;
+    if (!validacep.test(cep)) {
+        disableSpinner();
+        $("<div class='alert alert-warning alert-dismissible fade show' role='alert'>" +
+            "CEP " + $('#cep').val() + " inválido." +
+            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+            "<span aria-hidden='true'>&times;</span>" +
+            "</button>" +
+            "</div>").insertAfter('#cep-input');
+        $('#cep').val('');
+
+    }
+    else {
+        await $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+            if (dados.cep == undefined) {
+                disableSpinner();
+                $("<div class='alert alert-warning alert-dismissible fade show' role='alert'>" +
+                    "CEP " + $('#cep').val() + " não existe." +
+                    "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+                    "<span aria-hidden='true'>&times;</span>" +
+                    "</button>" +
+                    "</div>").insertAfter('#cep-input');
+                $('#cep').val('');
+            }
+            else {
+                let thead = $("#tabela thead");
+                if (thead.children().length == 0) {
+                    $("#tabela thead").append(
+                        "<tr>" +
+                        "<th scope='col'>CEP</th>" +
+                        "<th scope='col'>Logradouro</th>" +
+                        "<th scope='col'>Bairro</th>" +
+                        "<th scope='col'>Cidade</th>" +
+                        "<th scope='col'>Estado</th>" +
+                        "</tr>"
+                    );
+                }
+                $("#tabela tbody").append(
                     "<tr>" +
-                    "<th scope='col'>CEP</th>" +
-                    "<th scope='col'>Logradouro</th>" +
-                    "<th scope='col'>Bairro</th>" +
-                    "<th scope='col'>Cidade</th>" +
-                    "<th scope='col'>Estado</th>" +
+                    "<td>" + dados.cep + "</td>" +
+                    "<td>" + dados.logradouro + "</td>" +
+                    "<td>" + dados.bairro + "</td>" +
+                    "<td>" + dados.localidade + "</td>" +
+                    "<td>" + dados.uf + "</td>" +
                     "</tr>"
                 );
+                disableSpinner();
             }
-            $("#tabela tbody").append(
-                "<tr>" +
-                "<td>" + dados.cep + "</td>" +
-                "<td>" + dados.logradouro + "</td>" +
-                "<td>" + dados.bairro + "</td>" +
-                "<td>" + dados.localidade + "</td>" +
-                "<td>" + dados.uf + "</td>" +
-                "</tr>"
-            );
-        }
-    });
-    disableSpinner();
+        });
+    }
 }
 
 function enableSpinner() {
