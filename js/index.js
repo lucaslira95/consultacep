c313826528ab61711a2daa86c775216a7675de25
Lucas Lira <lucasgta95@gmail.com>
2020-02-04 16:05:51 -0300

    added combolist feature

--- js/index.js
+++ js/index.js
@@ -1,49 +1,58 @@
 document.getElementById("consultar").addEventListener("click", consultar);
-document.getElementById("cep").addEventListener("keypress", function (event) {
+document.getElementById("combolist").addEventListener("change", combolistSwitch);
+document.getElementById("cep").addEventListener("keypress", (event) => {checkEnterKey(event)});
+
+function checkEnterKey(event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         $('#consultar').click();
     }
-});
+}
 
-async function consultar() {
-    enableSpinner();
-    buttonSwitch("consultar", false);
-    let cep = $('#cep').val().replace(/\D/g, '');
-    var validacep = /^[0-9]{8}$/;
-
-    if (!validacep.test(cep)) {
-        disableSpinner();
-        insertAlert('#cep', 'inválido');
-        buttonSwitch("consultar", true);
-        $('#cep').val('');
-        $('#cep').focus();
-        $('.alert').on('close.bs.alert', function () {
-            $('#cep').focus();
-        })
+function combolistSwitch() {
+    if ($('#combolist').is(':checked')) {
+        enableCombolist();
     }
     else {
-        await $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
-            if (dados.cep == undefined) {
-                disableSpinner();
-                insertAlert('#cep', 'não existe');
-                buttonSwitch("consultar", true);
-                $('#cep').val('');
-                $('#cep').focus();
-                $('.alert').on('close.bs.alert', function () {
-                    $('#cep').focus();
-                })
-            }
-            else {
-
-                insertAdress(dados);
-                buttonSwitch("consultar", true);
-                disableSpinner();
-                $('#cep').val('');
-                $('#cep').focus();
-            }
-        });
+        disableCombolist();
+        document.getElementById("cep").addEventListener("keypress", (event) => {checkEnterKey(event)});
     }
+}
+
+async function consultar() {
+
+    enableSpinner();
+    buttonSwitch("consultar", false);
+    let cepList = $('#cep').val().split('\n');
+    for(let i = 0; i < cepList.length; i++){
+        let cep = cepList[i];
+        let filteredCep = cep.replace(/\D/g, '');
+        const validacep = /^[0-9]{8}$/;
+
+        if (!validacep.test(filteredCep)) {
+            insertAlert(cep, 'inválido');
+        }
+        else {
+            await $.getJSON("https://viacep.com.br/ws/" + filteredCep + "/json/?callback=?", function (dados) {
+                if (dados.cep == undefined) {
+                    insertAlert(cep, 'não existe');
+                }
+                else {
+                    insertAdress(dados);
+                }
+            });
+        }
+    }
+
+    buttonSwitch("consultar", true);
+    disableSpinner();
+    $('#cep').val('');
+    $('.alert').on('close.bs.alert', function () {
+        $('#cep').focus();
+    })
+    $('#cep').focus();
+
+
 }
 
 function enableSpinner() {
@@ -62,9 +71,9 @@ function buttonSwitch(tagId, state) {
     document.getElementById(tagId).enabled = state;
 }
 
-function insertAlert(tagId, suffixMessage) {
+function insertAlert(cep, suffixMessage) {
     $("<div class='alert alert-warning alert-dismissible fade show' role='alert'>" +
-        "CEP " + $(tagId).val() + " " + suffixMessage + "." +
+        "CEP " + cep + " " + suffixMessage + "." +
         "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
         "<span aria-hidden='true'>&times;</span>" +
         "</button>" +
@@ -93,5 +102,19 @@ function insertAdress(dados) {
         "<td>" + dados.localidade + "</td>" +
         "<td>" + dados.uf + "</td>" +
         "</tr>"
+    );
+}
+
+function disableCombolist() {
+    $('#cep').replaceWith(
+        "<input type='text' id='cep' class='form-control' placeholder='CEP'" +
+        "aria-label='CEP' aria-describedby='cepinput'></input>"
+    );
+}
+
+function enableCombolist() {
+    $('#cep').replaceWith(
+        "<textarea type='text' id='cep' class='form-control' placeholder='Lista de CEPs'" +
+        "aria-label='CEP' aria-describedby='cepinput'></textarea>"
     );
 }
