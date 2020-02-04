37b4a849a0a9d0cf6a4f69ffed64faf098ef6625
Lucas Lira <lucasgta95@gmail.com>
2020-02-04 12:28:24 -0300

    improved code reusability

--- js/index.js
+++ js/index.js
@@ -12,15 +12,9 @@ async function consultar() {
     let cep = $('#cep').val().replace(/\D/g, '');
     var validacep = /^[0-9]{8}$/;
 
-
     if (!validacep.test(cep)) {
         disableSpinner();
-        $("<div class='alert alert-warning alert-dismissible fade show' role='alert'>" +
-            "CEP " + $('#cep').val() + " inválido." +
-            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-            "<span aria-hidden='true'>&times;</span>" +
-            "</button>" +
-            "</div>").insertAfter('#cep-input');
+        insertAlert('#cep', 'inválido');
         buttonSwitch("consultar", true);
         $('#cep').val('');
         $('#cep').focus();
@@ -28,20 +22,11 @@ async function consultar() {
             $('#cep').focus();
         })
     }
-
-
     else {
-
         await $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
-
             if (dados.cep == undefined) {
                 disableSpinner();
-                $("<div class='alert alert-warning alert-dismissible fade show' role='alert'>" +
-                    "CEP " + $('#cep').val() + " não existe." +
-                    "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-                    "<span aria-hidden='true'>&times;</span>" +
-                    "</button>" +
-                    "</div>").insertAfter('#cep-input');
+                insertAlert('#cep', 'não existe');
                 buttonSwitch("consultar", true);
                 $('#cep').val('');
                 $('#cep').focus();
@@ -49,31 +34,9 @@ async function consultar() {
                     $('#cep').focus();
                 })
             }
-
             else {
-                let thead = $("#tabela thead");
 
-                if (thead.children().length == 0) {
-                    $("#tabela thead").append(
-                        "<tr>" +
-                        "<th scope='col'>CEP</th>" +
-                        "<th scope='col'>Logradouro</th>" +
-                        "<th scope='col'>Bairro</th>" +
-                        "<th scope='col'>Cidade</th>" +
-                        "<th scope='col'>Estado</th>" +
-                        "</tr>"
-                    );
-                }
-                $("#tabela tbody").append(
-                    "<tr>" +
-                    "<td>" + dados.cep + "</td>" +
-                    "<td>" + dados.logradouro + "</td>" +
-                    "<td>" + dados.bairro + "</td>" +
-                    "<td>" + dados.localidade + "</td>" +
-                    "<td>" + dados.uf + "</td>" +
-                    "</tr>"
-                );
-
+                insertAdress(dados);
                 buttonSwitch("consultar", true);
                 disableSpinner();
                 $('#cep').val('');
@@ -95,6 +58,40 @@ function disableSpinner() {
     $("#consultar").html('Consultar');
 }
 
-function buttonSwitch(element, state) {
-    document.getElementById(element).enabled = state;
+function buttonSwitch(tagId, state) {
+    document.getElementById(tagId).enabled = state;
+}
+
+function insertAlert(tagId, suffixMessage) {
+    $("<div class='alert alert-warning alert-dismissible fade show' role='alert'>" +
+        "CEP " + $(tagId).val() + " " + suffixMessage + "." +
+        "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+        "<span aria-hidden='true'>&times;</span>" +
+        "</button>" +
+        "</div>").insertAfter('#cep-input');
+}
+
+function insertAdress(dados) {
+    let thead = $("#tabela thead");
+
+    if (thead.children().length == 0) {
+        $("#tabela thead").append(
+            "<tr>" +
+            "<th scope='col'>CEP</th>" +
+            "<th scope='col'>Logradouro</th>" +
+            "<th scope='col'>Bairro</th>" +
+            "<th scope='col'>Cidade</th>" +
+            "<th scope='col'>Estado</th>" +
+            "</tr>"
+        );
+    }
+    $("#tabela tbody").append(
+        "<tr>" +
+        "<td>" + dados.cep + "</td>" +
+        "<td>" + dados.logradouro + "</td>" +
+        "<td>" + dados.bairro + "</td>" +
+        "<td>" + dados.localidade + "</td>" +
+        "<td>" + dados.uf + "</td>" +
+        "</tr>"
+    );
 }
